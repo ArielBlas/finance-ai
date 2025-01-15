cb891595cddc088b4230924eeac8cd37733e9748
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-01-14 22:34:42 -0300

    feat: user auth

--- src/components/landing-page/Header.tsx
+++ src/components/landing-page/Header.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="fixed top-0">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default Header;
